ca6df5e3e49aa0b8c271c6e0deae2a07b4f879c8
Juan Ignacio Bonini <juanignaciobonini@hotmail.com>
2024-09-18 09:14:03 -0300

    Se agrega clase 7.1

--- clase_7/apps/usuario/models.py
+++ clase_7/apps/usuario/models.py
@@ -7,3 +7,30 @@ class Usuario(models.Model):
 
     def __str__(self):
         return f"{self.id} - {self.username} - {self.telefono}"
+
+"""
+usuario_creado = Usuario.objects.create(username="Pedro", telefono="123456") # Usuario creado
+usuario_creado = Usuario(username="Pedro", telefono="123456").save()
+
+usuario_sin_crear = Usuario(username="Pedro", telefono="123456") # Usuario sin crear
+usuario_sin_crear.username
+usuario_sin_crear.telefono
+
+usuario_sin_crear.save()
+
+diccionario = {
+    "username": "Pedro",
+    "telefono": "123456"
+}
+usuario = Usuario(**diccionario)
+
+"""
+# def foo(**kwargs):
+#     print(kwargs)
+
+# foo(telefono="123456", username="Pedro")
+
+# def foo2(*args):
+#     print(args)
+
+# foo2("Pedro", "123456")
